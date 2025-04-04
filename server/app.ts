127d9b44bae4595f8374e8b3c353b79505c72ff6
adilarain00 <addilarain00@gmail.com>
2025-04-04 16:26:58 +0500

    solve user auth

--- server/app.ts
+++ server/app.ts
@@ -21,7 +21,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   Cors({
-    origin: "https://learn-x-elearning.vercel.app",
+    origin: ["https://learn-x-elearning.vercel.app"],
     credentials: true,
   })
 );
