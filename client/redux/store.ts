93898bbc00e98b6a429fcd83c48e29a0a07e293f
adilarain00 <addilarain00@gmail.com>
2025-04-08 15:30:24 +0500

    update refresh token

--- client/redux/store.ts
+++ client/redux/store.ts
@@ -16,7 +16,7 @@ export const store = configureStore({
 // call the refresh token function on every page loads
 const initializeApp = async () => {
   await store.dispatch(
-    apiSlice.endpoints.refershToken.initiate({}, { forceRefetch: true })
+    apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true })
   );
   await store.dispatch(
     apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
